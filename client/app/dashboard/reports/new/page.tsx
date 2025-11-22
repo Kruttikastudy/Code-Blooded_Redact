68ca14c233db8d7f1282e7e2683e2fc24558f081
KruttikaH <kruttikahebbar@gmail.com>
2025-11-23 02:25:59 +0530

    upload

--- client/app/dashboard/reports/new/page.tsx
+++ client/app/dashboard/reports/new/page.tsx
@@ -76,7 +76,7 @@ export default function AddReportPage() {
         }, 1500);
 
         try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/analyze`, {
+            const response = await fetch(`http://127.0.0.1:8000/api/analyze`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
